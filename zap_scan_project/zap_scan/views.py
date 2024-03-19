ce85d088fd406c582ea2243c01c8618da55c47e5
M-JV <melvinjohn2001@gmail.com>
2024-03-19 07:39:41 +0530

    web-scan works

--- zap_scan_project/zap_scan/views.py
+++ zap_scan_project/zap_scan/views.py
@@ -1,8 +1,28 @@
-# zap_scan/views.py
-
 from django.shortcuts import render
 from django.http import JsonResponse
-from .scan_script import run_scan
+from zapv2 import ZAPv2
+import time
+
+def run_scan(target_url):
+    api_key = "n8u0gb8krmkokc7du1vc0gbg2l"
+    zap = ZAPv2(apikey=api_key)
+
+    print("Starting spider scan...")
+    scan_id = zap.spider.scan(target_url)
+    while int(zap.spider.status(scan_id)) < 100:
+        print("Spider progress: {}%".format(zap.spider.status(scan_id)))
+        time.sleep(5)
+    print("Spider scan completed.")
+
+    print("Starting active scan...")
+    scan_id = zap.ascan.scan(target_url)
+    while int(zap.ascan.status(scan_id)) < 100:
+        print("Active scan progress: {}%".format(zap.ascan.status(scan_id)))
+        time.sleep(5)
+    print("Active scan completed.")
+
+    alerts = zap.core.alerts()
+    return alerts
 
 def scan_view(request):
     if request.method == 'POST':
