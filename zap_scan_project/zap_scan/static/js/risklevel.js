bab313b25a184e4efc1f43fda727515facd085f8
M-JV <melvinjohn2001@gmail.com>
2024-04-30 15:52:43 +0530

    lookda

--- zap_scan_project/zap_scan/static/js/risklevel.js
+++ zap_scan_project/zap_scan/static/js/risklevel.js
@@ -37,12 +37,6 @@ function getProgressBarValues() {
     const currentProgressBar1Value = parseInt(progressBar1.getAttribute('data-max-value'), 10);
     const currentProgressBar2Value = parseInt(progressBar2.getAttribute('data-max-value'), 10);
 
-    // Get the values of the progress bars (assuming you have a method to get the current values)
-    // This is a placeholder for the actual implementation, as you would need to update these values
-    // according to how the progress bars are being used in your application.
-    // const currentProgressBar1Value = 5; // Example value (you can replace this with actual value retrieval)
-    // const currentProgressBar2Value = 5; // Example value (you can replace this with actual value retrieval)
-
     // Call the function to update the risk level based on the progress bar values
     updateRiskLevel(currentProgressBar1Value, currentProgressBar2Value);
 }
